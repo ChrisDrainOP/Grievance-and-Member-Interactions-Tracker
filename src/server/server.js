e9c7139b4c7429a1e1d37c5f84e52e514d908f9e
Chrislearns <drainc4@yahoo.com>
2021-01-24 20:23:34 -0800

    add .env, connected mongodb, understand env
    I now understand the importance of a .env file. I also was able to
    create a mongodb path in my system enviromental variables. Updated git
    ignore to not include the .env file

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const server = express();
 const port = process.env.PORT || 5000;
+require("dontenv").config();
 
 const cors = require("cors");
 const corsOptions = {
   origin: "http://localhost:3000",
 };
 
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+mongoose.connect(MONGO_URL);
 server.listen(port, () => {
   console.log(`Listening on ${port}`); //eslint-disable-line no-console
 });
