7ac29b938b80f05d2881283db05cbe77a937e069
Chrislearns <drainc4@yahoo.com>
2021-02-01 05:20:59 -0800

    deleted saveSubTask Comp, redid state
    Seperating out the elements inside the AddSubTask component was
    unneccesary. It prevents the state from being properly manage and the
    form from reseting. Also change the hidden element feature from a
    javascript one to a CSS implementation

--- src/client/components/AddSubTask.js
+++ src/client/components/AddSubTask.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import SaveSubTask from "./SaveSubTask";
 
 const AddSubTask = () => {
-  const [showSubTask, setShowSubTask] = useState(false);
-  const handleSaveSubTaskButtons = () => setShowSubTask(true);
+  const [showSubTask, setShowSubTask] = useState("hidden");
+  const handleSubTaskDisplay = () => setShowSubTask(true);
+
+  const [subTaskValue, setSubTaskValue] = useState("");
+  const handleSubTaskValueChange = (e) => setSubTaskValue(e.target.value);
 
   return (
     <div className='mt-2'>
@@ -16,9 +18,33 @@ const AddSubTask = () => {
           type='text'
           name='subtask'
           id='subtask'
-          onClick={handleSaveSubTaskButtons}
+          value={subTaskValue}
+          onChange={handleSubTaskValueChange}
+          onClick={handleSubTaskDisplay}
         />
-        {showSubTask ? <SaveSubTask /> : null}
+        <div className={`${showSubTask}`}>
+          <label htmlFor='reminder'>Remind Me</label>
+          <input
+            className='block bg-white'
+            type='date'
+            name='reminder'
+            id='reminder'
+          />
+          <div className='pt-3'>
+            <div className=' space-x-1 '>
+              <input
+                className='w-2/5 bg-blue-400 p-1 '
+                type='submit'
+                value='Save'
+              />
+              <input
+                className='w-2/5 bg-indigo-400 p-1'
+                type='reset'
+                onClick={() => setShowSubTask("hidden")}
+              />
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
