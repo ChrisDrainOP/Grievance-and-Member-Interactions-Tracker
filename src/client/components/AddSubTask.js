3ba77454e3b34d336a2f0be3c2f0395dac30011b
Chrislearns <drainc4@yahoo.com>
2021-01-31 21:41:33 -0800

    Subtask onclick appear functional
    Added the subtask remind, save, and reset area onclick feature. When the
    subtask input box is clicked the SaveSubTask component appears.

--- src/client/components/AddSubTask.js
+++ src/client/components/AddSubTask.js
@@ -1,32 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SaveSubTask from "./SaveSubTask";
 
 const AddSubTask = () => {
+  const [showSubTask, setShowSubTask] = useState(false);
+  const handleSaveSubTaskButtons = () => setShowSubTask(true);
+
   return (
-    <div className=''>
+    <div className='mt-2'>
       <form>
-        <label className='block w-full text-left' htmlFor='subtask'>
+        <label className='block w-full text-left font-bold' htmlFor='subtask'>
           Add Subtask
         </label>
-        <input className='w-full' type='text' name='subtask' id='subtask' />
-        <div>
-          <label htmlFor='reminder'>Remind Me</label>
-          <input
-            className='block bg-white'
-            type='date'
-            name='reminder'
-            id='reminder'
-          />
-          <div className='pt-3'>
-            <div className=' space-x-1'>
-              <input
-                className='w-2/5 bg-blue-400 p-1 '
-                type='submit'
-                value='Save'
-              />
-              <input className='w-2/5 bg-indigo-400 p-1' type='reset'></input>
-            </div>
-          </div>
-        </div>
+        <input
+          className='w-full'
+          type='text'
+          name='subtask'
+          id='subtask'
+          onClick={handleSaveSubTaskButtons}
+        />
+        {showSubTask ? <SaveSubTask /> : null}
       </form>
     </div>
   );
