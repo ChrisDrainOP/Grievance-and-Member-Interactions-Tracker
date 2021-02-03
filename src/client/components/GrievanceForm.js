7f118b947daabae7325e2b72fb42c1b85a4e3ac6
Chrislearns <drainc4@yahoo.com>
2021-02-03 03:42:41 -0800

    Grievance Form Component Redone.
    Form now represents new DB Schema. Next will try to post info to the DB
    from form.

--- src/client/components/GrievanceForm.js
+++ src/client/components/GrievanceForm.js
@@ -5,50 +5,88 @@ import AddSubTask from "./AddSubTask";
 
 const GrievanceForm = (prop) => {
   return (
-    <div className='mx-3 pt-5 relative  h-full'>
-      <input
-        type='submit'
-        className='text-1xl p-2 absolute right-0 top-2 bg-blue-400 border-2 rounded-lg'
-        value='Save'
-      />
-      <form className='text-center'>
-        <div className='w-3/4  pb-4 text-left'>
-          <label htmlFor='add-a-task' className='inline-block'>
+    <div className='mx-2 pt-5 relative h-full'>
+      <form action='/add-meeting' method='POST' className='text-center'>
+        <input
+          type='submit'
+          className='text-1xl p-2 absolute right-0 top-2 bg-blue-400 border-2 rounded-lg'
+          value='Save'
+        />
+        <div className='  pb-4 text-left'>
+          <label htmlFor='meetingName' className='inline-block'>
             <FontAwesomeIcon icon={faGripLinesVertical} />
           </label>
           <input
             type='text'
-            name='add-a-task'
-            value='Grievance Description'
-            className='inline-block bg-blue-100 pl-1'
+            name='meetingName'
+            placeholder='Meeting Name Here'
+            className='inline-block bg-blue-100 pl-1 font-bold'
           />
         </div>
-        <div className='w-3/4 '>
-          <label className='block text-left w-full' htmlFor='tasks'>
-            Event Type:{" "}
-          </label>
-          <select className='block bg-white' name='task type' id='tasks'>
-            <option value=''>Select type of task</option>
-            <option value='Member Interaction'>Member Interaction</option>
-            <option value='Incident'>Incident</option>
-            <option value='Step 1'>Step 1</option>
-            <option value='Step 2'>Step 2</option>
-            <option value='Step 2 to Arbitration'>Step 2 to Arbitration</option>
-            <option value='Step 3 Appeal'>Step 3 Appeal</option>
-            <option value='Information Request (RFI)'>
-              Information Request (RFI)
-            </option>
-            <option value='Miscellaneous'>Miscellaneous</option>
-          </select>
+        <div className='mt-3 w-full flex space-x-3'>
+          <div className='relative top-6'>
+            <label className='block text-left font-bold' htmlFor='tasks'>
+              Event Type:{" "}
+            </label>
+            <select
+              className='bg-white w-full block h-7 '
+              name='task type'
+              id='tasks'
+            >
+              <option value='Member Interaction'>Member Interaction</option>
+              <option value='Incident'>Incident</option>
+              <option value='Step 1'>Step 1</option>
+              <option value='Step 2'>Step 2</option>
+              <option value='Step 2 to Arbitration'>
+                Step 2 to Arbitration
+              </option>
+              <option value='Step 3 Appeal'>Step 3 Appeal</option>
+              <option value='Information Request (RFI)'>
+                Information Request (RFI)
+              </option>
+              <option value='Miscellaneous'>Miscellaneous</option>
+            </select>
+          </div>
+          <div className=''>
+            <label className='text-left block font-bold' htmlFor='meetingDate'>
+              Actual Date of Meeting:
+            </label>
+            <input
+              className='w-full block'
+              type='date'
+              name='meetingDate'
+              id='meetingDate'
+            />
+          </div>
         </div>
-        <div className='w-3/4 '>
-          <label htmlFor='date' className='block w-full text-left'>
-            Date:
-          </label>
-          <input type='date' name='date' className='block bg-white' />
+        <div className='flex mt-3 relative w-full space-x-3'>
+          <div className='relative top-6'>
+            <label htmlFor='reminder' className='block text-left font-bold'>
+              Reminder:
+            </label>
+            <input
+              type='date'
+              name='reminder'
+              className='w-full bg-white block'
+            />
+          </div>
+          <div className='text-left'>
+            <label className='block font-bold' htmlFor='extension'>
+              Extension Date:<span className='block'>(If Applicable)</span>
+            </label>
+            <input
+              className='w-full block'
+              type='date'
+              name='extension'
+              id='extension'
+            />
+          </div>
         </div>
-        <div className='w-3/4 '>
-          <label className='block w-full text-left' htmlFor='description'>
+        <div className='mt-3 '>
+          <label
+            className='block w-full text-left font-bold'
+            htmlFor='description'
+          >
             Description:
           </label>
           <textarea
