5dd5957d8a10949027ee09ca5acc99ab15b72320
Chrislearns <drainc4@yahoo.com>
2021-02-13 14:40:41 -0800

    Google Auth20 home redirect working
    After adding the port 3000 to the .env file and putting this variable in
    the google callback uri I'm now being redirected to the home page after
    signing in with google.

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -4,13 +4,10 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   createdAt: {
     type: Date,
+    default: Date.now,
     required: true,
   },
   googleId: {
-    type: String,
-    required: true,
-  },
-  emails: {
     type: String,
     required: true,
   },
