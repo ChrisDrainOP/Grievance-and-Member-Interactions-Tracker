ea1e8119619c9e0d89b020c306c7176a4c6970fe
Chrislearns <drainc4@yahoo.com>
2021-03-21 03:51:04 -0700

    Finished New Grievance Table List
    The list will seperate the form function of naming and choosing a
    meeting type in order to have custom forms for each meeting type a user
    may choose.

--- src/client/containers/GrievanceTableContainer.js
+++ src/client/containers/GrievanceTableContainer.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import GrievanceFormContainer from "./GrievanceFormContainer";
 
 const GrievanceTableContainer = (props) => {
+  const [showAddNewForm, setShowAddNewForm] = useState('hidden')
+  const [isAddNewClicked, setIsAddNewClicked] = useState(false);
+  const handleIsAddNewClicked = () => {
+    setIsAddNewClicked(true)
+    setShowAddNewForm("block")
+  }
+  const handleIsResetClicked = () => {
+    setIsAddNewClicked(false)
+    setShowAddNewForm("hidden")
+  }
   let events = props.listType;
-
 
   const listOfEvents = events.map((event) => {
     function eventDate(date) {
@@ -14,7 +25,7 @@ const GrievanceTableContainer = (props) => {
       <li
         onClick={() => props.handleEventRowClick(event)}
         key={event._id}
-        className='my-2 border w-full'
+        className='my-2 w-full'
       >
         <label
           for={event._id}
@@ -27,7 +38,6 @@ const GrievanceTableContainer = (props) => {
               id={`${event._id}`}
               value={`${event._id}`}
               className='mr-3'
-
             />
           </span>
           {event.meetingName}
@@ -45,22 +55,72 @@ const GrievanceTableContainer = (props) => {
   });
 
   return (
-    <div className='h-screen bg-white'>
+    <div className='h-screen bg-blue-300'>
+      <div className='flex py-4'>
+        <div className='border-black ml-4'>
+          <button className='mb-2' onClick={handleIsAddNewClicked}>
+            <FontAwesomeIcon className='block relative left-6' icon={faPlus} />
+            <span className='font-bold block'>Add New</span>
+          </button>
+          <form className={`${showAddNewForm}`}>
+            <div className='text-left'>
+              <label htmlFor='meetingName' className='inline-block'></label>
+              <input
+                type='text'
+                name='meetingName'
+                placeholder='Add Event Name'
+                className='inline-block bg-white mb-2 pl-1 font-bold'
+                onClick=""
+                id='meetingName'
+              />
+            </div>
+            <div className=''>
+              <label
+                className='block text-left font-bold'
+                htmlFor='tasks'
+              ></label>
+              <select className='bg-white ' name='meetingType' id='meetingType'>
+                <option value=''>Event Type?</option>
+                <option value='Member Interaction'>Member Interaction</option>
+                <option value='Incident'>Incident</option>
+                <option value='Step 1'>Step 1</option>
+                <option value='Step 2'>Step 2</option>
+                <option value='Step 2 to Arbitration'>
+                  Step 2 to Arbitration
+                </option>
+                <option value='Step 3 Appeal'>Step 3 Appeal</option>
+                <option value='Miscellaneous'>Miscellaneous</option>
+              </select>
+              <button
+                className='bg-blue-400 ml-3 px-2 p-1 rounded-lg '
+                type='submit'
+              >
+                Save
+              </button>
+              <button
+              onClick={handleIsResetClicked} type='reset' className='bg-purple-400 rounded-lg p-1 px-2 ml-2'>Reset</button>
+            </div>
+          </form>
+        </div>
+      </div>
       <div className='text-md font-bold ml-5'>
         <h3 className='inline-block'>
           {props.listName ? props.listName : "View Meetings"}
         </h3>
       </div>
+
       <div>
         <ol className='w-full' id='checkboxes'>
-          <div className='border w-full flex'>
+          <div className=' w-full flex'>
             <h6 className='w-1/2 text-center'>Description</h6>
             <h6 className='w-1/2 text-center'>Date of Event</h6>
           </div>
           {listOfEvents}
         </ol>
       </div>
-      {props.selectedEvent ?<GrievanceFormContainer selectedEvent={props.selectedEvent} /> : null}
+      {props.selectedEvent ? (
+        <GrievanceFormContainer selectedEvent={props.selectedEvent} />
+      ) : null}
     </div>
   );
 };
